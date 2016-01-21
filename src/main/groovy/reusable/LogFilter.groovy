220214c8d28894255aa5e5705152a572f5c4d497
Rocky DeVries <rocky.devries@target.com>
2016-01-21 11:14:35 -0600

    Cast for clarity

--- src/main/groovy/reusable/LogFilter.groovy
+++ src/main/groovy/reusable/LogFilter.groovy
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component
 
 import javax.servlet.*
 import javax.servlet.annotation.WebFilter
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletRequestWrapper
 
 @WebFilter(urlPatterns = '/*')
@@ -16,7 +17,7 @@ class LogFilter implements Filter {
   void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     String body = request.inputStream.text
     log.debug("Request body: $body")
-    chain.doFilter(new HttpServletRequestWrapper(request) {
+    chain.doFilter(new HttpServletRequestWrapper(request as HttpServletRequest) {
       @Override
       ServletInputStream getInputStream() throws IOException {
         return new ReusableServletInputStream(new ByteArrayInputStream(body.bytes))
